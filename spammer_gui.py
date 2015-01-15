1bbac5ab9af25945f1c4325b38119c8dc9f1320a
Kevin Chen <k.chen8880@gmail.com>
2015-01-15 04:03:18 -0500

    So that's how you add tabs!

--- spammer_gui.py
+++ spammer_gui.py
@@ -1,35 +1,75 @@
 import sys
+
+# Pyside imports
 from PySide.QtCore import *
 from PySide.QtGui import *
 from PySide.QtDeclarative import QDeclarativeView
 
+# Spammer imports
+import quip_db as qdb
+
+# Hotkey tool
+import pyhk
+
+
 '''
 Main window
-Use sub-windows for separate tasks
+Put everything else in tabs
 '''
 class MainWindow(QDeclarativeView):
 	
-	def __init__(self, parent = None):
+	def __init__(self, starting_app = None, parent = None):
 		super(MainWindow, self).__init__(parent)
 		self.setWindowTitle("Chat Spammer")
 		
-		# Render the .qml file for the view
-		self.setSource(QUrl.fromLocalFile("spammer_view.qml"))
+		# Initialize library (not ready for testing)
+		#self.lib = qdb.Library(starting_app, pyhk.pyhk())
 		
-		# Resize QML window with outer window
-		self.setResizeMode(QDeclarativeView.SizeRootObjectToView)
+		# Make the tab area
+		self.tab = Tab(self)
+		self.tab.addTab(QWidget(), "Main")
+		self.tab.addTab(QWidget(), "Main2")
+		self.tab.show()
 '''
 End of MainWindow definition
 '''
 
- 
+
+'''
+Containers for all tabs
+Single tab for each category
+'''
+class Tab(QTabWidget):
+	
+	def __init__(self, parent = None):
+		super(Tab, self).__init__(parent)
+'''
+End of Tab definition
+'''
+
+
+'''
+Dialog boxes for tab contents
+
+Existing quips: Contains a textbox for the text and a textbox for keystrokes
+	Make a new one for each quip found in the database
+New quip: Contains a single empty textbox for text input and a single empty textbox for keystroke input
+	Make one of these after all quips are loaded
+'''
+class TabContent():
+	pass
+'''
+End of Tab Content definition
+'''
+
+
 '''
 Run as main for testing only
 '''
 if __name__ == '__main__':
 	# Create Qt application and the QDeclarative view
 	app = QApplication(sys.argv)
-	window = MainWindow()
+	window = MainWindow("MOBA")
 	window.show()
 	
 	# Enter Qt main loop
